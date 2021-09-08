f44c27f5898f26a1a3e4dc30692a6a08050f6ac8
Sergey Kiselev <kissergey98@mail.ru>
2021-09-08 22:26:41 +0300

    navbar adding

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,64 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="header">
+    <div class="header__logo">
+
+      <div class="header__logo_logo-text">
+        <span class="tri">TRI</span>
+        <span class="va">VA</span>
+        <span class="go">GO</span>
+      </div>
+    </div>
+    <div class="header__links links">
+      <div class="links__item">О нас</div>
+      <div class="links__item">
+        Услуги
+        <div class="links__item_dropdown">
+        </div>
+      </div>
+      <div class="links__item">Вакансии</div>
+      <div class="links__item">Новости</div>
+      <div class="links__item">
+        Соглашения
+        <div class="links__item_dropdown">
+        </div>
+      </div>
+    </div>
+    <div class="header__buttons">
+      <div class="wrapper-header__buttons_signup">
+        <div class="header__buttons_signup">
+          ВОЙТИ
+        </div>
+      </div>
+      <div class="header__buttons_signin">
+        ЗАРЕГИСТРИРОВАТЬСЯ
+      </div>
+    </div>
+  </div>
+  <div class="content">
+
+  </div>
+
+  <div class="footer">
+    <div class="footer-upper">
+
+    </div>
+    <div class="footer-lower">
+
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
   components: {
-    HelloWorld
+
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+@import url('style.css');
+
 </style>
